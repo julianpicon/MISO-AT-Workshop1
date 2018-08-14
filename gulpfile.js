271d5a64757b6ff6b799d4627fed4cd494ac5176
Julian <julian.picon@hotmail.com>
2018-08-13 21:03:51 -0500

    Integración SWPrecache

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var pagespeed = require('pse');
+var reload = browserSync.reload;
+
+gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+  var rootDir = 'public';
+
+  swPrecache.write(path.join(rootDir, 'sw.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
+    stripPrefix: rootDir
+  }, callback);
+});
